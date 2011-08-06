e602df2cce602bf7c674441b16cc447fc6994f8e
Chad Kittel <chad.kittel@gmail.com>
2011-08-06 16:34:06 -0500

    moved to src/ dir

---
+++ src/HoiPolloi/Collections/DictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HoiPolloi.Collections
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+        {
+            if(dictionary == null) throw new ArgumentNullException("dictionary");
+
+            TValue value;
+            return dictionary.TryGetValue(key, out value) ? value : defaultValue;
+        }
+    }
+}
---
+++ src/HoiPolloi/SortDirection.cs
@@ -0,0 +1,18 @@
+﻿namespace HoiPolloi
+{
+    /// <summary>
+    /// Specifies the direction in which a list can be sorted.
+    /// </summary>
+    public enum SortDirection
+    {
+        /// <summary>
+        /// Sort from smallest to largest.  For example, from A to Z or 0 to 100.
+        /// </summary>
+        Ascending = 0,
+
+        /// <summary>
+        /// Sort from largest to smallest.  For example, from Z to A or 100 to 0.
+        /// </summary>
+        Descending
+    }
+}
